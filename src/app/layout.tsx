8d9dc9309feda0da861118d3c1598f62fccd3657
Atharva Lole <167648917+AtharvaLole@users.noreply.github.com>
2026-04-02 00:34:58 +0530

    Delete src directory

--- src/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "./globals.css";
-
-export default function RootLayout({ children }: any) {
-  return (
-    <html>
-      <body className="bg-gray-100">{children}</body>
-    </html>
-  );
-}
