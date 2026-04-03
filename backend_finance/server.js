09d8e675bd15f62f2b235ad98774a7b96fe4a46b
Atharva Lole <167648917+AtharvaLole@users.noreply.github.com>
2026-04-03 13:56:21 +0530

    Update server.js

--- backend_finance/server.js
+++ backend_finance/server.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
+const cors = require("cors"); 
 
 connectDB();
+
+app.use(cors());
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
